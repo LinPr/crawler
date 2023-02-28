a34acd85b40439549e5c5bd6c8c1f5c7a6fc3fa1
LinPr <314573849@qq.com>
2023-02-28 06:13:44 -0800

    brower fetcher and set User-Agent

--- main.go
+++ main.go
@@ -1,64 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io"
-	"net/http"
+	"github.com/LinPr/crawler/collect"
 	"regexp"
+	"time"
 )
 
+var url string = "https://book.douban.com/subject/1007305/"
+var rgxPattern = `<div class=[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`
+
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
+
+	// var f collect.Fetcher = collect.BaseFetch{}
+	var f collect.Fetcher = collect.BrowerFetch{
+		Timeout: time.Second * 3,
+	}
+
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Printf("Fetch err: %v\n", err)
 		return
 	}
 
-	// fmt.Println(string(body))
-	rgx := regexp.MustCompile(`<div class=[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`)
+	fmt.Println(string(body))
+
+	rgx := regexp.MustCompile(rgxPattern)
 	matches := rgx.FindAllStringSubmatch(string(body), -1)
 	for _, v := range matches {
 		fmt.Println(v)
 		fmt.Println()
 	}
 
-}
-
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("http.Get(url) err: %v\n", err)
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("resp.StatusCode: %v\n", resp.StatusCode)
-	}
-
-	bodyReader := bufio.NewReader(resp.Body)
-
-	e := DetermineEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return io.ReadAll(utf8Reader)
-
-	// fmt.Printf("body: %v\n", string(body))
-}
-
-func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
-	//检测html页面编码使用peek
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		fmt.Printf("r.Peek(1024) err: %v\n", err)
-		return unicode.UTF8
-	}
-
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
